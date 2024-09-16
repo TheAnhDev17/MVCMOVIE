71d56362fcc643d52f5ee6e2fb6e1eeb8dc3e002
theanh.dev17@gmail.com <theanh.dev17@gmail.com>
2024-09-16 23:34:16 +0700

    Add search title

--- MVCMOVIE/Models/Movie.cs
+++ MVCMOVIE/Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCMOVIE.Models;
 
@@ -6,8 +8,11 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
+    [DisplayName("Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
